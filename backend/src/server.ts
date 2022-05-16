de83e5f96444086f45239b2ef46fdd4f82bcca73
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-05-16 11:56:58 -0300

    NEW: Readme and change console in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import app from './app'
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at port ${PORT}`);
 });
